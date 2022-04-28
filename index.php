4ebab95543c4ac502f76f878736c19c0de8e1545
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2022-04-29 00:40:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <?php
- $code1 = $_REQUEST['bilangan_1']
- $code2 = $_REQUEST['bilangan_2']
+ $bilangan_1 = $_REQUEST['bilangan_1'];
+ $bilangan_2 = $_REQUEST['bilangan_2'];
 
-echo $code1 + $code2 = $tambah;
-echo $code1 - $code2 = $kurang;
-
+ $tambah = $bilangan_1+$bilangan_2;
+ $kurang = $bilangan_1-$bilangan_2;
+ ?>
 <html>
     <body>
-    <form name= form2 >
+    <form name= "form1" action="">
         <table width="400" border="1">
             <tr>
-                <td colspan="2" width="400" align="center">perhintungan_bilangan</td>
+                <td colspan="2" width="200" align="center">hasil</td>
             </tr>
             <tr>
-                <td width="200"><?=$code1;?>+<?=$code2;?></td>
+                <td width="200"><?=$bilangan_1;?>+<?=$bilangan_2;?></td>
                 <td><?=$tambah;?></td>
             </tr>
             <tr>
-                <td width="200"><?=$code1;?>-<?=$code2></td>
+                <td width="200"><?=$bilangan_1;?>-<?=$bilangan_2;?></td>
                 <td><?=$kurang;?></td>
             </tr>
             <tr>
-                <td colspan="2" width="400"></td>
+                <td colspan="2" width="400" align="center">===</td>
             </tr>
         </table>
     </form>
     <body>
 <html>
-?>
