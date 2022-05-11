6cb0cca943abd1af1e42ff4abf0261ca54c35208
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2022-05-11 18:47:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
- $bilangan_1 = $_REQUEST['bilangan_1'];
- $bilangan_2 = $_REQUEST['bilangan_2'];
+ $bilangan_1 = $_REQUEST['code1'];
+ $bilangan_2 = $_REQUEST['code2'];
 
- $tambah = $bilangan_1+$bilangan_2;
- $kurang = $bilangan_1-$bilangan_2;
+ $tambah = $bilangan_1 + $bilangan_2;
+ $kurang = $bilangan_1 - $bilangan_2;
+ $kali = $bilangan_1 * $bilangan_2;
  ?>
 <html>
     <body>
-    <form name= "form1" action="">
+       <form>
         <table width="400" border="1">
             <tr>
                 <td colspan="2" width="200" align="center">hasil</td>
@@ -21,9 +22,14 @@
                 <td><?=$kurang;?></td>
             </tr>
             <tr>
+                <td width="200"><?=$bilangan_1;?>*<?=$bilangan_2;?></td>
+                <td><?=$kali;?></td>
+            </tr>
+            <tr>
                 <td colspan="2" width="400" align="center">===</td>
             </tr>
         </table>
-    </form>
-    <body>
-<html>
+      </form>
+    </body>
+</html>
+
